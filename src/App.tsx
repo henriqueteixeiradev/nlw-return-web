03b2348ce4238db94848d2e77cd391e5a3c401e2
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2022-05-08 09:05:32 -0300

    aplicação do tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="bg-violet-500 py-5 flex justify-center items-center">
+      <h1
+        className="text-lg text-gray-50 font-bold
+      "
+      >
+        Hello World Maria
+      </h1>
+    </div>
+  );
 }
 
 export default App;
