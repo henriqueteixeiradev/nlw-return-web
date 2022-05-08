0ed6c01161843f22682c360f4db31c9b22575789
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2022-05-08 12:14:59 -0300

    parte 3 do widgetform, criando botões de feedback e mapeando

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,5 +1,7 @@
 import { CloseButton } from "./CloseButton";
 
+import { feedbackTypes } from "../mock/feedbackTypes";
+
 export function WidgetForm() {
   return (
     <div
@@ -27,7 +29,42 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <p>Hello World</p>
+      <div
+        className={`
+          flex
+          py-8
+          gap-2
+          w-full
+      `}
+      >
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button
+              className={`
+                bg-zinc-800
+                rounded-lg
+                py-5
+                w-24
+                flex-1
+                flex
+                flex-col
+                items-center
+                gap-2
+                border-2
+                border-transparent
+                hover:border-brand-500
+                focus:border-brand-500
+                focus:outline-none
+            `}
+              key={key}
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
 
       <footer
         className={`
