eb4166ea7062d038035019ce43ff28ea52ab7fbc
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2022-05-08 11:19:48 -0300

    adicionando elementos  no widget form parte 1

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -6,13 +6,13 @@ export function Widget() {
   return (
     <Popover
       className={`
-        absolute 
+        absolute
         bottom-4
         right-4
         md:bottom-8
         md:right-8
-        flex 
-        flex-col 
+        flex
+        flex-col
         items-end
     `}
     >
@@ -22,23 +22,23 @@ export function Widget() {
 
       <Popover.Button
         className={`
-        bg-brand-500 
-          rounded-full 
-          px-3 h-12 
-        text-white 
-          flex 
-          items-center 
+        bg-brand-500
+          rounded-full
+          px-3 h-12
+        text-white
+          flex
+          items-center
           group
       `}
       >
         <ChatTeardropDots className="w-6 h-6" />
         <span
           className={`
-            max-w-0 
-            overflow-hidden 
-            group-hover:max-w-xs 
-            transition-all 
-            duration-500 
+            max-w-0
+            overflow-hidden
+            group-hover:max-w-xs
+            transition-all
+            duration-500
             ease-linear
         `}
         >
--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -2,20 +2,47 @@ export function WidgetForm() {
   return (
     <div
       className={`
-      bg-zinc-900 
-        p-4 
-        relative 
-        rounded-2xl 
-        mb-4 
-        flex 
-        flex-col 
-        items-center 
-        shadow-lg 
-        w-[calc(100vw-2rem)] 
+      bg-zinc-900
+        p-4
+        relative
+        rounded-2xl
+        mb-4
+        flex
+        flex-col
+        items-center
+        shadow-lg
+        w-[calc(100vw-2rem)]
         md:w-auto
     `}
     >
-      <h1>Hello World</h1>
+      <header
+        className={`
+          text-xl
+          leading-6
+        `}
+      >
+        <span>Deixe seu Feedback</span>
+      </header>
+
+      <p>Hello World</p>
+
+      <footer
+        className={`
+          text-xs
+          text-neutral-400
+      `}
+      >
+        Feito com ♥ pela{" "}
+        <a
+          className={`
+            underline
+            underline-offset-2
+        `}
+          href="https:rocketseat.com.br/"
+        >
+          Rocketseat
+        </a>
+      </footer>
     </div>
   );
 }
